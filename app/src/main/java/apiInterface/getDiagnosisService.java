eda99840cb308bf67574f9b48952cee34d9f8199
PetrosVal <petrosva12gmail.com>
2019-11-24 23:07:54 +0200

    Initialize-Diagnosis-layout

---
+++ app/src/main/java/apiInterface/getDiagnosisService.java
@@ -0,0 +1,13 @@
+package apiInterface;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+
+public interface getDiagnosisService {
+}
--- app/src/main/java/com/example/ambulanceconsulting/activities/DiagnosisActivity.java
+++ app/src/main/java/com/example/ambulanceconsulting/activities/DiagnosisActivity.java
@@ -2,6 +2,8 @@ package com.example.ambulanceconsulting.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -14,16 +16,24 @@ import java.util.ArrayList;
 public class DiagnosisActivity extends AppCompatActivity {
 
     private ArrayList<String> symptomIds;
+    private EditText year , gender;
+    private Button diagnosis;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnosis);
 
+        year = (EditText) findViewById(R.id.year);
+        gender = (EditText) findViewById(R.id.gender);
+        diagnosis =(Button) findViewById(R.id.btnDiagnosis);
+
         symptomIds = new ArrayList<>();
         Intent i = getIntent();
         symptomIds = i.getStringArrayListExtra("ids");
-        Toast.makeText(DiagnosisActivity.this,String.valueOf(symptomIds.size()),Toast.LENGTH_SHORT).show();
+       // Toast.makeText(DiagnosisActivity.this,String.valueOf(symptomIds.size()),Toast.LENGTH_SHORT).show();
+
+
 
     }
 
